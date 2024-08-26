487e7236bfda383bdb9ab326b224c9b4383a8807
Equan P. <equan.p@gmail.com>
2024-08-26 10:29:29 +0700

    add services template

---
+++ app/services/gridDBService.js
@@ -0,0 +1,40 @@
+/**
+ * Need to be fixed later
+ * 
+const griddb = require('griddb-node-sdk');
+
+const factory = new griddb.StoreFactory.getInstance();
+const store = factory.getStore({
+	host: "your-host",
+	port: 10001,
+	clusterName: "your-cluster-name",
+	username: "admin",
+	password: "admin"
+});
+*/
+
+async function getDocumentaryMetadata(docId) {
+	/**
+		const container = await store.getContainer("documentary_metadata");
+		const query = container.query(`SELECT * WHERE id = '${docId}'`);
+		const rs = await query.fetch();
+		if (rs.hasNext()) {
+			return rs.next();
+		}
+	
+	**/
+	return null;
+}
+
+async function saveDocumentaryMetadata(metadata) {
+	/**
+	const container = await store.getContainer("documentary_metadata");
+	await container.put(metadata);
+	*/
+	return null
+}
+
+export {
+	getDocumentaryMetadata,
+	saveDocumentaryMetadata,
+};
---
+++ app/services/openAIService.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function generateNarrative(prompt) {
+	//code here
+	return prompt
+}
+
+async function generateTitle(prompt) {
+	// code here
+	return prompt;
+}
+
+export {
+	generateNarrative,
+	generateTitle,
+}

10b9fac78b4d31d5bd481ca2d6f7c478341a59d3
Equan P. <equan.p@gmail.com>
2024-09-06 09:37:03 +0700

    change the max tokens

--- app/services/openAIService.js
+++ app/services/openAIService.js
@@ -4,14 +4,36 @@ const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 })
 
-async function generateTitle(prompt) {
-	// code here
-	return prompt
+async function generateTitle(narrative) {
+	const titleResponse = await openai.chat.completions.create({
+		model: 'gpt-4o-mini',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a professional title generator.'
+			},
+			{
+				role: 'user',
+				content: `Direct answer only. Generate a title for the following narrative text: \n${narrative}`
+			}
+		],
+		temperature: 1,
+		max_tokens: 1000,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+		response_format: {
+			"type": "text"
+		},
+	})
+
+	const title = titleResponse.choices[0].message.content
+	return title
 }
 
-async function generateNarrative(frames, audioTranscription) {
+async function generateNarrative(frames) {
 	const videoDuration = 2
-	
+
 	const frameObjects = frames.map(x => ({
 		type: 'image_url',
 		image_url: {
@@ -37,7 +59,14 @@ async function generateNarrative(frames, audioTranscription) {
 			},
 			videoContent
 		],
-		temperature: 0,
+		temperature: 1,
+		max_tokens: 4095,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+		response_format: {
+			"type": "text"
+		},
 	});
 
 	console.log(response)
@@ -46,7 +75,7 @@ async function generateNarrative(frames, audioTranscription) {
 }
 
 async function generateVoice(narrative) {
- return narrative
+	return narrative
 }
 
 export { generateNarrative, generateTitle, generateVoice }
